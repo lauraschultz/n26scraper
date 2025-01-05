b29ea00ba842c4d4ce68392a865e7feddf9aaf2b
lauraschultz <schultzlaurac@gmail.com>
2025-01-05 14:14:36 +0100

    handle 'today' and 'yesterday' date labels

--- content-script.js
+++ content-script.js
@@ -1,4 +1,4 @@
-const dateFormat = /^\w*-(\d*)-(\w*)-\d*$/;
+const dateFormat = /^(\w*-(\d*)-(\w*)-\d*)|(yesterday-\d*)|(today-\d*)$/;
 const price = /-?.\d*\.\d{2}$/;
 
 const months = {
@@ -16,22 +16,43 @@ const months = {
 	12: "december",
 };
 
+const today = new Date();
+const yesterday = new Date(today.getTime() - 24 * 60 * 60 * 1000);
+
 const getMonth = (month, year) => {
 	let csv = "";
 	const s = document.querySelectorAll("section");
 	s.forEach((section) => {
-		if (section.id?.includes(months[month])) {
+		if (
+			section.id?.includes(months[month]) ||
+			section.id?.includes("today") ||
+			section.id?.includes("yesterday")
+		) {
 			// section represents one date
 			const match = section.id.match(dateFormat);
+			// if date: matches[2] contains day of month
+			// if 'yesterday': matches[4]
+			// if 'today': matches[5]
+
 			const transactions = section.querySelectorAll("li");
 			transactions.forEach((t) => {
 				const divs = t.querySelectorAll("li > div");
-				const date = `${match[1]}.${month}.${year || new Date().getFullYear()}`;
+				const includeToday = today.getMonth() + 1 === month;
+				const includeYesterday = yesterday.getMonth() + 1 === month;
+				let date;
+				if (match[2]) {
+					date = `${match[2]}.${month}.${year || today.getFullYear()}`;
+				} else if (includeToday && match[5]) {
+					date = `${today.getDate()}.${month}.${today.getFullYear()}`;
+				} else if (includeYesterday && match[4]) {
+					date = `${yesterday.getDate()}.${month}.${today.getFullYear()}`;
+				}
+
 				const name = divs[1].textContent;
 				const amount = divs[2].textContent.match(price)?.[0];
 				// console.log(divs[2].textContent);
 
-				if (!name.match(/Declined$/)) {
+				if (date && !name.match(/Declined$/)) {
 					csv = `${date},${name},${amount}\n` + csv;
 				}
 			});
@@ -44,7 +65,6 @@ const getMonth = (month, year) => {
 chrome.runtime.onMessage.addListener(
 	({ month, year }, sender, sendResponse) => {
 		const result = getMonth(month, year);
-		console.log({ result });
 		sendResponse({ errorMessage: null, result });
 	}
 );
